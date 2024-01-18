425de13f13d71c043c77014f5103f27bdf9afd07
zwxiao1 <50433859+zwxiao1@users.noreply.github.com>
2024-01-18 15:14:50 +0900

    Add files via upload

---
+++ メタバース/avatar/Assets/ChangeColorOnKeyPress.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColorOnKeyPress : MonoBehaviour
+{
+    // 定义要改变颜色的UI对象
+    private Image bgImage;
+
+    void Start()
+    {
+        bgImage = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            bgImage.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
